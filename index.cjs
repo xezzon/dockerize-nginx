75ad3a0d7b48ce726eca42bbb943c6b0d473eb5d
xezzon <xezzon@outlook.com>
2023-12-15 22:29:21 +0800

    静态资源走协商缓存

--- index.cjs
+++ index.cjs
@@ -60,6 +60,10 @@ server {
       try_files $uri /index.html;
   }
 
+  location ~* \.(html|css|js|png|jpg|jpeg|gif|gz|svg|mp4|ogg|ogv|webm|htc|xml|woff)$ {
+    add_header  Cache-Control  no-cache;
+  }
+
   error_page   500 502 503 504  /50x.html;
   location = /50x.html {
       root   /usr/share/nginx/html;
